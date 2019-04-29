1ac9dcc2a5bec3510df23cd8e095706d71cb017a
discuzben <17759500581@189.cn>
2019-04-29 23:45:46 +0800

    API请求可选品牌

---
+++ js/screeningCondition.js
@@ -0,0 +1,270 @@
+var screeningCondition = {
+	brand: [{
+		value: 'A',
+		label: 'A',
+		children: [{value:'阿尔法罗密欧',label:'阿尔法罗密欧'},{value:'AMC',label:'AMC'},{value:'奥迪',label:'奥迪'},{value:'奥兹莫比尔',label:'奥兹莫比尔'},{value:'阿斯顿马丁',label:'阿斯顿马丁'}],
+		},{
+		value: 'B',
+		label: 'B',
+		children: [{value:'宝马',label:'宝马'},{value:'保时捷',label:'保时捷'},{value:'奔驰',label:'奔驰'},{value:'本田',label:'本田'},{value:'标致',label:'标致'},{value:'别克',label:'别克'},{value:'宾利',label:'宾利'},],
+		},{
+		value: 'D',
+		label: 'D',
+		children: [{value:'道奇RAM',label:'道奇RAM'},{value:'道奇',label:'道奇'},{value:'达特桑',label:'达特桑'},{value:'大宇',label:'大宇'},{value:'大众',label:'大众'},{value:'德劳瑞恩',label:'德劳瑞恩'},],
+		},{
+		value: 'F',
+		label: 'F',
+		children: [],
+		},{
+		value: 'G',
+		label: 'G',
+		children: [],
+		},{
+		value: 'H',
+		label: 'H',
+		children: [],
+		},{
+		value: 'I',
+		label: 'I',
+		children: [],
+		},{
+		value: 'J',
+		label: 'J',
+		children: [],
+		},{
+		value: 'K',
+		label: 'K',
+		children: [],
+		},{
+		value: 'L',
+		label: 'L',
+		children: [],
+		},{
+		value: 'M',
+		label: 'M',
+		children: [],
+		},{
+		value: 'O',
+		label: 'O',
+		children: [],
+		},{
+		value: 'P',
+		label: 'P',
+		children: [],
+		},{
+		value: 'Q',
+		label: 'Q',
+		children: [],
+		},{
+		value: 'R',
+		label: 'R',
+		children: [],
+		},{
+		value: 'S',
+		label: 'S',
+		children: [],
+		},{
+		value: 'T',
+		label: 'T',
+		children: [],
+		},{
+		value: 'w',
+		label: 'w',
+		children: [],
+		},{
+		value: 'X',
+		label: 'X',
+		children: [],
+		},{
+		value: 'Y',
+		label: 'Y',
+		children: [],
+		}],
+	carseries: [{
+		value: '不限',
+		label: '不限'
+	},{
+		value: '宝马 3系',
+		label: '宝马 3系'
+	},{
+		value: '奔驰 C系',
+		label: '奔驰 C系'
+	},{
+		value: '丰田 凯美瑞',
+		label: '丰田 凯美瑞'
+	},{
+		value: '宝马 5系',
+		label: '宝马 5系'
+	},{
+		value: '奔驰 E系',
+		label: '奔驰 E系'
+	},{
+		value: '福特 野马',
+		label: '福特 野马'
+	},{
+		value: '保时捷 卡宴',
+		label: '保时捷 卡宴'
+	},{
+		value: '本田 雅阁',
+		label: '本田 雅阁'
+	},{
+		value: '丰田 卡罗拉',
+		label: '丰田 卡罗拉'
+	},{
+		value: '本田 思域',
+		label: '本田 思域'
+	}],
+	cartype: [{
+			value: '不限',
+			label: '不限'
+		},{
+			value: '轿车',
+			label: '轿车'
+		},{
+			value: '跑车',
+			label: '跑车'
+		},{
+			value: '皮卡',
+			label: '皮卡'
+		},{
+			value: 'SUV',
+			label: 'SUV'
+		},{
+			value: 'MPV',
+			label: 'MPV'
+		}],
+	price: [{
+			value: '',
+			label: '不限'
+		},{
+			value: '1',
+			label: '$10,000以下'
+		},{
+			value: '2',
+			label: '$10,000-$15,000'
+		},{
+			value: '3',
+			label: '$15,000-$20,000'
+		},{
+			value: '4',
+			label: '$20,000-$30,000'
+		},{
+			value: '5',
+			label: '$30,000-$40,000'
+		},{
+			value: '6',
+			label: '$40,000以上'
+		}],
+	mileage: [{
+			value: '',
+			label: '不限'
+		},{
+			value: '1',
+			label: '1万英里以内'
+		},{
+			value: '2',
+			label: '3万英里以内'
+		},{
+			value: '3',
+			label: '5万英里以内'
+		},{
+			value: '4',
+			label: '8万英里以内'
+		},{
+			value: '5',
+			label: '10万英里以内'
+		},{
+			value: '6',
+			label: '15万英里以内'
+		}],
+	age: [{
+			value: '',
+			label: '不限'
+		},{
+			value: '1',
+			label: '1年以内'
+		},{
+			value: '2',
+			label: '3年以内'
+		},{
+			value: '3',
+			label: '5年以内'
+		},{
+			value: '4',
+			label: '6年以内'
+		},{
+			value: '5',
+			label: '8年以内'
+		},{
+			value: '6',
+			label: '8年以上'
+		}],
+	gearbox: [{
+			value: '',
+			label: '不限'
+		},{
+			value: '手动',
+			label: '手动'
+		},{
+			value: '自动',
+			label: '自动'
+		},{
+			value: '手自一体',
+			label: '手自一体'
+		},{
+			value: '无级变速',
+			label: '无级变速'
+		},{
+			value: '双离合',
+			label: '双离合'
+		},{
+			value: '机械自动变速箱',
+			label: '机械自动变速箱'
+		},{
+			value: '直驱',
+			label: '直驱'
+		}],
+	color: [{
+			value: '',
+			label: '不限'
+		},{
+			value: '白色',
+			label: '白色'
+		},{
+			value: '黑色',
+			label: '黑色'
+		},{
+			value: '银灰',
+			label: '银灰'
+		},{
+			value: '灰色',
+			label: '灰色'
+		},{
+			value: '黄色',
+			label: '黄色'
+		},{
+			value: '香槟',
+			label: '香槟'
+		},{
+			value: '红色',
+			label: '红色'
+		},{
+			value: '粉色',
+			label: '粉色'
+		},{
+			value: '紫色',
+			label: '紫色'
+		},{
+			value: '蓝色',
+			label: '蓝色'
+		},{
+			value: '绿色',
+			label: '绿色'
+		},{
+			value: '咖啡',
+			label: '咖啡'
+		},{
+			value: '其他',
+			label: '其他'
+		}],
+}
