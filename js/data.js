a0010987f73b0cd0addd73d52317fd6f0c468a39
discuzben <17759500581@189.cn>
2019-05-05 00:17:59 +0800

    detail page add preorder

--- js/data.js
+++ js/data.js
@@ -20,5 +20,29 @@ var data = {
 				console.log('请求出错了!' + data);
 			}
 		});
-	}
+	},
+	commonheader: function(url,fun,form){
+		var thatdata = this;
+		$.ajax({
+			url: thatdata.domain + url,
+			method: 'post' ,
+			headers: {
+				'Authorization':localStorage.token
+			},
+			data: form,
+			success(data) {
+				if(data.code == 200){
+					fun(data.data);
+				}else if(data.code == 400){
+					fun(data.code);
+				}
+				else{
+					console.log(data);
+				}
+			},
+			error(data){
+				console.log('请求出错了!' + data);
+			}
+		});
+	},
 }
