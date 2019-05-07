b2578f58a9c28bb0fc3e83aabe00b9722cbe713b
discuzben <1281696080@qq.com>
2019-05-07 20:30:47 +0800

    index and sale page add collect ,personcenter add update personal info

--- js/data.js
+++ js/data.js
@@ -2,49 +2,49 @@ var data = {
 	domain: 'http://admin.championcargroup.com/api/pc/',
 	common: function(url,fun,form){
 		var thatdata = this;
-		$.ajax({
-			url: thatdata.domain + url,
-			method: 'post' ,
-			data: form,
-			success(data) {
-				if(data.code == 200){
-					fun(data.data);
-				}else if(data.code == 400){
-					fun(data);
+		if(!localStorage.token){
+			$.ajax({
+				url: thatdata.domain + url,
+				method: 'post' ,
+				data: form,
+				success(data) {
+					if(data.code == 200){
+						fun(data.data);
+					}else if(data.code == 400){
+						fun(data);
+					}
+					else{
+						console.log(data);
+					}
+				},
+				error(data){
+					console.log('请求出错了!' + data);
 				}
-				else{
-					console.log(data);
+			});
+		}else{
+			$.ajax({
+				url: thatdata.domain + url,
+				method: 'post' ,
+				headers: {
+					'Authorization':localStorage.token
+				},
+				data: form,
+				success(data) {
+					if(data.code == 200){
+						// console.log(data)
+						fun(data.data);
+					}else if(data.code == 400){
+						fun(data);
+						console.log(data)
+					}
+					else{
+						console.log(data);
+					}
+				},
+				error(data){
+					console.log('请求出错了!' + data);
 				}
-			},
-			error(data){
-				console.log('请求出错了!' + data);
-			}
-		});
-	},
-	commonheader: function(url,fun,form){
-		var thatdata = this;
-		$.ajax({
-			url: thatdata.domain + url,
-			method: 'post' ,
-			headers: {
-				'Authorization':localStorage.token
-			},
-			data: form,
-			success(data) {
-				if(data.code == 200){
-					// console.log(data)
-					fun(data.data);
-				}else if(data.code == 400){
-					fun(data);
-					console.log(data)
-				}
-				else{
-					console.log(data);
-				}
-			},
-			error(data){
-				console.log('请求出错了!' + data);
-			}
-		});
-	},
+			});
+		}
+	}
 }
