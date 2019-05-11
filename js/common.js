572d1f3c90ef0438ce6bc2fbf1a7fb1f8752b1a3
discuzben <17759500581@189.cn>
2019-05-11 10:55:37 +0800

    update index page switch language

--- js/common.js
+++ js/common.js
@@ -8,3 +8,98 @@ $(function(){
 		}
 	});
 })
+var orifun = {
+	getInfo:function (messageen,messagecn){
+		var message;
+		if(localStorage.locale == 'en'){
+			message = messageen;
+		}else{
+			message = messagecn;
+		}
+		return message;
+	},
+	initStatu: function(obj){
+		if(locale){
+			i18n.locale = locale;
+			locale == 'zh-cn'?obj.enshow = 'english':obj.enshow = '中文';
+		}else{
+			(locale = i18n.locale);
+		}
+		var login;
+		 if(localStorage.login){
+			login = JSON.parse(localStorage.login);
+		 }
+		if(login){
+			obj.switchlogin = false;
+		}else{
+			obj.switchlogin = true;
+		}
+	},
+	login: function(obj){
+		if(obj.logindata.username == ''){
+			// alert("请填写用户名!");
+			obj.$Message.warning('请填写用户名!');
+			return;
+		}
+		else if(obj.logindata.password == ''){
+			// alert("请填写密码!");
+			obj.$Message.warning('请填写密码!');
+			return;
+		}
+		data.common('Login/login',function(d){
+			// that.slideArr = d;
+			if(d != 400){
+				// console.log(d)
+				localStorage.token = d;
+				obj.switchlogin = !obj.switchlogin;
+				obj.loginshow = !obj.loginshow;
+				obj.$Notice.success({
+					title: '登录成功! ',
+				});
+				localStorage.login = true;
+			}else{
+				obj.$Message.warning('登录失败,请检查用户名或密码是否正确!');
+			}
+		},obj.logindata)
+	},
+	register:function(obj){
+		if(obj.registerdata.username == ''){
+			// alert("请填写用户名!");
+			obj.$Message.warning('请填写用户名!');
+			return;
+		}
+		else if(obj.registerdata.password == ''){
+			// alert("请填写密码!");
+			obj.$Message.warning('请填写密码!');
+			return;
+		}
+		data.common('Login/register',function(d){
+				if(d == 400 || d.code == 400){
+					obj && obj.$Message.warning('此用户名已被使用，请更换!');
+				}else{
+					obj.$Notice.success({
+						title: '注册成功!去登陆>>',
+					});
+				}
+		},obj.registerdata)
+	},
+	consult:function(obj){
+		obj.botshow = !obj.botshow;
+		setTimeout(function(){
+			obj.$Notice.info({
+				title: orifun.getInfo('Fully functional!','功能完善中!'),
+			});
+		},700)
+	},
+	change :function(obj) {
+		obj.enshow = obj.enshow == 'english' ? "中文" : "english";
+		setTimeout(function(){
+			if(i18n.locale == 'en'){
+				i18n.locale = 'zh-cn';
+			}else if(i18n.locale == 'zh-cn'){
+				i18n.locale = 'en';
+			}
+			localStorage.locale = i18n.locale;
+		},1100)
+	},
+};
--- js/language.js
+++ js/language.js
@@ -28,13 +28,7 @@
 				seltype: ['Latest-hot','ECO-Car','Prospective-new-car','luxury-car'],
 				explain: [],
 				mediaVoice: 'Media voice',
-				media: [{
-					title: 'New York motor show 2018: all-new subaru forest man debuts',
-					art: 'Hyundai motor co. Will launch 8 new or redesigned crossover multipurpose vehicles by 2020. The Santa Fe family will also launch an 8-seat SUV in the future, which will be named after the brand new name.The 2019 Santa Fe will have a long-wheelbase version with seven seats. The new car will have a three-row seat layout and will be called the Santa Fe XL. Hyundai also said it will introduce an eight-seat SUV with a new name.In terms of power, the Santa Fe family will offer a choice of 2.4l and 2.0t engines, with a maximum power of 185hp and 235hp respectively.In addition, a 2.2LCRDi turbocharged diesel engine with 190 horsepower output will be provided, which will be matched by an 8-speed automatic transmission.'
-				},{
-					title: 'New York motor show 2018: all-new subaru forest man debuts',
-					art: 'Hyundai motor co. Will launch 8 new or redesigned crossover multipurpose vehicles by 2020. The Santa Fe family will also launch an 8-seat SUV in the future, which will be named after the brand new name.The 2019 Santa Fe will have a long-wheelbase version with seven seats. The new car will have a three-row seat layout and will be called the Santa Fe XL. Hyundai also said it will introduce an eight-seat SUV with a new name.In terms of power, the Santa Fe family will offer a choice of 2.4l and 2.0t engines, with a maximum power of 185hp and 235hp respectively.In addition, a 2.2LCRDi turbocharged diesel engine with 190 horsepower output will be provided, which will be matched by an 8-speed automatic transmission.'
-				}],
+				media: [],
 				misc: [{"content":'Service agreement','src':"serivceAgreement.html"},
 				{"content":'Privacy statement','src':"private.html"},
 				{"content":'Suggestion feedback','src':"feedback.html"},
@@ -51,6 +45,7 @@
 				scom: "'s comment",
 				strongRecom: 'is highly recommended',
 				score: 'score',
+				address: 'Massachusetts state',
 			},
 		    'zh-cn': {
 				header: {
@@ -78,23 +73,7 @@
 					seltype: ['最新最热','经济实惠','准新车','豪华车'],
 					explain: [],
 					mediaVoice: '媒体声音',
-					media: [{
-						title: '2018纽约车展：全新斯巴鲁森林人亮相',
-						art: '日前，有海外媒体发布了一组2019款现代Santa Fe的图片，据悉，现代汽车公司直至2020年，将推出8款全新或重新设计的跨界多用途车型，Santa Fe系列未来也将推出一款8座SUV车型，并将以全新名称进行命名。据悉，2019款Santa Fe将推出一款长轴距版7座车型，新车将采用三排座椅布局，并命名为Santa Fe XL，此外现代汽车公司表示还将推出一款8座版SUV车型，并将采用全新名称进行命名。力方面，Santa Fe系列产品将提供2.4L发动机以及2.0T发动机可供选择，其最大功率分别为185马力、235马力。此外，还将提供一款可输出190马力的2.2LCRDi涡轮增压柴油机，传动方面与其匹配的均为8速自动变速箱。'
-					},{
-						title: '2018纽约车展：全新斯巴鲁森林人亮相',
-						art: '日前，有海外媒体发布了一组2019款现代Santa Fe的图片，据悉，现代汽车公司直至2020年，将推出8款全新或重新设计的跨界多用途车型，Santa Fe系列未来也将推出一款8座SUV车型，并将以全新名称进行命名。据悉，2019款Santa Fe将推出一款长轴距版7座车型，新车将采用三排座椅布局，并命名为Santa Fe XL，此外现代汽车公司表示还将推出一款8座版SUV车型，并将采用全新名称进行命名。力方面，Santa Fe系列产品将提供2.4L发动机以及2.0T发动机可供选择，其最大功率分别为185马力、235马力。此外，还将提供一款可输出190马力的2.2LCRDi涡轮增压柴油机，传动方面与其匹配的均为8速自动变速箱。'
-					},{
-						title: '2018纽约车展：全新斯巴鲁森林人亮相',
-						art: '日前，有海外媒体发布了一组2019款现代Santa Fe的图片，据悉，现代汽车公司直至2020年，将推出8款全新或重新设计的跨界多用途车型，Santa Fe系列未来也将推出一款8座SUV车型，并将以全新名称进行命名。据悉，2019款Santa Fe将推出一款长轴距版7座车型，新车将采用三排座椅布局，并命名为Santa Fe XL，此外现代汽车公司表示还将推出一款8座版SUV车型，并将采用全新名称进行命名。力方面，Santa Fe系列产品将提供2.4L发动机以及2.0T发动机可供选择，其最大功率分别为185马力、235马力。此外，还将提供一款可输出190马力的2.2LCRDi涡轮增压柴油机，传动方面与其匹配的均为8速自动变速箱。'
-					},{
-						title: '2018纽约车展：全新斯巴鲁森林人亮相',
-						art: '日前，有海外媒体发布了一组2019款现代Santa Fe的图片，据悉，现代汽车公司直至2020年，将推出8款全新或重新设计的跨界多用途车型，Santa Fe系列未来也将推出一款8座SUV车型，并将以全新名称进行命名。据悉，2019款Santa Fe将推出一款长轴距版7座车型，新车将采用三排座椅布局，并命名为Santa Fe XL，此外现代汽车公司表示还将推出一款8座版SUV车型，并将采用全新名称进行命名。力方面，Santa Fe系列产品将提供2.4L发动机以及2.0T发动机可供选择，其最大功率分别为185马力、235马力。此外，还将提供一款可输出190马力的2.2LCRDi涡轮增压柴油机，传动方面与其匹配的均为8速自动变速箱。'
-					},{
-						title: '2018纽约车展：全新斯巴鲁森林人亮相',
-						art: '日前，有海外媒体发布了一组2019款现代Santa Fe的图片，据悉，现代汽车公司直至2020年，将推出8款全新或重新设计的跨界多用途车型，Santa Fe系列未来也将推出一款8座SUV车型，并将以全新名称进行命名。据悉，2019款Santa Fe将推出一款长轴距版7座车型，新车将采用三排座椅布局，并命名为Santa Fe XL，此外现代汽车公司表示还将推出一款8座版SUV车型，并将采用全新名称进行命名。力方面，Santa Fe系列产品将提供2.4L发动机以及2.0T发动机可供选择，其最大功率分别为185马力、235马力。此外，还将提供一款可输出190马力的2.2LCRDi涡轮增压柴油机，传动方面与其匹配的均为8速自动变速箱。'
-					},
-					],
+					media: [],
 					misc: [{"content":'服务协议','src':"serivceAgreement.html"},
 					{"content":'隐私声明','src':"private.html"},
 					{"content":'意见反馈','src':"feedback.html"},
@@ -111,6 +90,7 @@
 					scom: '的评价',
 					strongRecom: '强烈推荐',
 					score: '分',
+					address: '马塞诸萨州',
 			}
 		}// 设置地区信息
 	})
