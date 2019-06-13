ddb242ca14552cf0de1050d7408051b8ccbd7ea7
discuzben <17759500581@189.cn>
2019-06-13 10:18:44 +0800

    update redirect

--- js/common.js
+++ js/common.js
@@ -20,21 +20,22 @@ var orifun = {
 	},
 	initStatu: function(obj){
 		function browserRedirect() {
-			var sUserAgent = navigator.userAgent.toLowerCase();
-			var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-			var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-			var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-			var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-			var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-			var bIsAndroid = sUserAgent.match(/android/i) == "android";
-			var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-			var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-			if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+			// var sUserAgent = navigator.userAgent.toLowerCase();
+			// var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+			// var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+			// var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+			// var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+			// var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+			// var bIsAndroid = sUserAgent.match(/android/i) == "android";
+			// var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+			// var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+			var w = document.body.clientWidth;
+			if (w<800) {
 				//跳转移动端页面
 				window.location.href="http://m.championcargroup.com";
 			} else {
 				//跳转pc端页面
-				window.location.href="http://www.championcargroup.com";
+				// window.location.href="http://www.championcargroup.com";
 			}
 		}
 		browserRedirect();
